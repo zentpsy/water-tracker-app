6745c24ef4f9038f61f74f723df1aff123821fc2
zentpsy <zentpsy@gmail.com>
2025-07-16 17:27:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ st.markdown(
         padding: 12px 36px;
         border-radius: 8px;
         border: none;
-        min-width: 150px;
+        min-width: 100px;
         transition: background-color 0.3s ease;
     }
     div.save-button > button:hover {
